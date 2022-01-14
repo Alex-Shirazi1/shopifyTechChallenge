6bb83308e82e4fcbbb5d1d10b10b28399a696fd5
Alex Shirazi <alexshirazi@Alexs-Mac-mini.local>
2022-01-13 23:35:30 -0800

    simplifying app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
-import View from './pages/View';
 const App=()=>{
 
 
@@ -12,20 +11,14 @@ const App=()=>{
     <div>
       <nav>
       <Link to="/"> Home </Link>
-      <Link to="/view"> View </Link>
+     
 
       </nav>
-      <Switch>
-      
-      
-        <Route path="/view">
-          <View />
-        </Route>
+   
         <Route path="/">
           <Home />
         </Route>
-       
-      </Switch>
+
     </div>
   );
   }
